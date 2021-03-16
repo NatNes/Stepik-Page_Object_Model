b958e2383fd69ae944c8acb35e123117039779e1
Nesterova Natalia <natalia_nv@mail.ru>
2021-03-16 22:44:33 +0300

    Локаторы страницы вынесены в отдельный файл

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,15 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):    # В аргументы больше не надо передавать экземпляр браузера, мы его передаем
                                    # и сохраняем на этапе создания Page Object. Вместо него нужно указать аргумент
                                    # self , чтобы иметь доступ к атрибутам и методам класса
         # login_link = self.browser.find_element_by_css_selector("#login_link")
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.is_element_present(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid"),\
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK),\
             "Login link is not presented"
