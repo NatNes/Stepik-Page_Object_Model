6a8f279c0d2a66bf6ec94a5112d6d0fbdf7459f0
Nesterova Natalia <natalia_nv@mail.ru>
2021-03-16 22:02:01 +0300

    Добавлена обработка исключений

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
-    def __init__(self, browser, url):   #конструктор — метод, который вызывается, когда мы создаем объект.
-                                        # В него в качестве параметров передаем экземпляр драйвера и url адрес.
+    def __init__(self, browser, url, timeout=10):   #конструктор — метод, который вызывается, когда мы создаем объект.
+                                                    # В него в качестве параметров передаем экземпляр драйвера и url адрес.
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)  # неявное ожидание
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,14 @@
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
 class MainPage(BasePage):
     def go_to_login_page(self):    # В аргументы больше не надо передавать экземпляр браузера, мы его передаем
                                    # и сохраняем на этапе создания Page Object. Вместо него нужно указать аргумент
                                    # self , чтобы иметь доступ к атрибутам и методам класса
-        login_link = self.browser.find_element_by_css_selector("#login_link")
+        # login_link = self.browser.find_element_by_css_selector("#login_link")
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid"),\
+            "Login link is not presented"
